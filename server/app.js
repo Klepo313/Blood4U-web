711867c22834612a408a349257bb3268cda06e57
TheKlepo75 <antonio.klepo1@gmail.com>
2022-04-06 23:09:55 +0200

    commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+
+require('dotenv').config(); 
+
+var express = require('express');
+var cors = require('cors');
+var app = express();
+const pool = require('./db');
+
+const port = 3000;
+
+app.use(cors());
+
+app.get("/login/:email/:password", pool.loginUser)
+
+app.listen(port, () => {
+    var host = "localhost";
+    console.log(`Server listening on port http://%s:%s`, host, port);
+})
---
+++ server/db.js
@@ -0,0 +1,59 @@
+var pg = require('pg');
+//var connectionString = "postgres://qulaynxc:zwIMATnDC4p_yujWhgC1ccg1x6XpgRab@abul.db.elephantsql.com/qulaynxc";
+var connectionString = "postgres://ejrdmfdk:axOzhBbUpcr7pgoboEnj7qVI7TZQQzL5@kandula.db.elephantsql.com/ejrdmfdk"
+var pool = new pg.Client(connectionString);
+
+pool.connect()
+
+const loginUser = (request, response) => {
+    var email = request.params.email;
+    var passwd = request.params.password; 
+    
+    if(email && passwd){
+        console.log("Pridani parametri: ")
+        console.log("EMAIL: " + email + "\tPASSWORD: " + passwd)
+    
+        pool.query(`select k.kor_id, k.kor_ime, k.kor_prezime, k.kor_krvnagrupa , k.kor_spol, k.kor_email from korisnici k , korisnicki_podaci kp 
+                    where kp.kpd_id = k.kor_id 
+                    and kp.kpd_email = '${email}'
+                    and kp.kpd_password = '${passwd}' `, (err, results) => {
+
+            if(err) throw err; //U slučaju greške
+
+            else if(typeof(results.rows[0])!=="undefined"){
+
+                var kor_id = results.rows[0].kor_id
+                var kor_ime_prezime = results.rows[0].kor_ime + ' ' + results.rows[0].kor_prezime
+                var kor_kg = results.rows[0].kor_krvnagrupa
+                var kor_email = results.rows[0].kor_email
+
+                //response.status(200).json(results.rows);
+                response.status(200).json({
+                    "status": "successful", 
+                    "kor_id": kor_id, 
+                    "kor_ime": kor_ime_prezime, 
+                    "kor_kg": kor_kg, 
+                    "kor_email": kor_email
+                })
+
+            } else{
+                console.log("Greška 2")
+                response.status(200).json({"status":"unsuccessful"}); 
+            }
+
+                }
+            )
+    
+    } else{
+        console.log("Greška 1")
+        response.status(200).json({"status":"unsuccessful"}); 
+    }
+
+}
+
+
+
+module.exports = {
+    pool,
+    loginUser,
+} 
