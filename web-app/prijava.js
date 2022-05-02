0ce4c25959196b13d49cc317c6df8fc40a0e9427
TheKlepo75 <antonio.klepo1@gmail.com>
2022-05-02 12:33:14 +0200

    commit

---
+++ web-app/prijava.js
@@ -0,0 +1,31 @@
+var email = document.getElementById('inpEmail');
+var password = document.getElementById('inpPass');
+
+var form = document.getElementsByTagName("form")[0]
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+function login() {
+    if(email.value!=null && password.value!=null){
+        fetch("http://localhost:3000/login/" + email.value + "/" + password.value)
+        .then( response => {
+            response.json().then(parsedJson => {
+                console.log(parsedJson)
+
+                console.log(parsedJson.kor_id)
+
+                if(parsedJson.status === "unsuccessful"){
+                    email.style.border = "3px solid #D22F27"
+                    password.style.border = "3px solid #D22F27"
+                } else{
+                    location.href = "map.html";
+                }
+
+            })
+        })
+    } else {
+        alert("Unesi podatke u tražena polja")
+    }
+
+}
+
