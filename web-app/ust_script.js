9e6bc2fc6920d2d5d435c5ed5dacc4f1c28f87e0
TheKlepo75 <antonio.klepo1@gmail.com>
2022-05-03 09:18:40 +0200

    commit

--- web-app/ust_script.js
+++ web-app/ust_script.js
@@ -1,4 +1,9 @@
 var ustanova = sessionStorage.getItem("element")
+var h1 = document.getElementsByTagName("h1")[0]
+var h2 = document.getElementsByTagName("h2")[0]
+var telefon = document.getElementsByClassName("tel")[0]
+var email = document.getElementsByClassName("email")[0]
+var potrebna_krv = document.getElementsByClassName("potrebna_krv")
 
 if(ustanova === "Firule"){
     console.log(ustanova)
