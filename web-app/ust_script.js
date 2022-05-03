fdd1db8f0af576ef11b59734bda7f34aec1f6159
TheKlepo75 <antonio.klepo1@gmail.com>
2022-05-03 09:23:58 +0200

    commit

--- web-app/ust_script.js
+++ web-app/ust_script.js
@@ -3,10 +3,22 @@ var h1 = document.getElementsByTagName("h1")[0]
 var h2 = document.getElementsByTagName("h2")[0]
 var telefon = document.getElementsByClassName("tel")[0]
 var email = document.getElementsByClassName("email")[0]
-var potrebna_krv = document.getElementsByClassName("potrebna_krv")
+var potrebna_krv = document.getElementsByClassName("krv")
 
 if(ustanova === "Firule"){
-    console.log(ustanova)
+    h1.innerHTML = "KBC Firule, Split"
+    h2.innerHTML = "Put Firula, 21000 - Split"
+    telefon.innerHTML = "021 556 111"
+    email.innerHTML = "Ectm@kbsplit.hr"
+    potrebna_krv[0].innerHTML = "A,"
+    potrebna_krv[1].innerHTML = "B,"
+    potrebna_krv[2].innerHTML = "0-"
 } else{
-    console.log(ustanova)
+    h1.innerHTML = "KBC Križine, Split"
+    h2.innerHTML = "Šoltanska 1, 21000 - Split"
+    telefon.innerHTML = "021 557 111"
+    email.innerHTML = "Ectm@kbsplit.hr"
+    potrebna_krv[0].innerHTML = "A,"
+    potrebna_krv[1].innerHTML = "AB+,"
+    potrebna_krv[2].innerHTML = "0"
 }
